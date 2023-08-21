a43ac46ba76cb947bcfec6cee5eac5581025da3f
Kenneth Fechter <kenneth.fechter@gmail.com>
2023-08-21 19:47:26 -0400

    Add Project

---
+++ app/src/main/java/com/kennethfechter/lab551/Login/LoginActivity.kt
@@ -0,0 +1,136 @@
+package com.kennethfechter.lab551.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.ActionCodeSettings
+import com.google.firebase.auth.FirebaseAuth
+import com.kennethfechter.lab551.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        if(FirebaseAuth.getInstance().currentUser == null) {
+            createSignInIntent()
+        } else {
+            // Load Mainpage
+        }
+
+    }
+
+    private fun createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.AppleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_create_intent]
+    }
+
+    // [START auth_fui_result]
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
+    private fun themeAndLogo() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+
+        // [START auth_fui_theme_logo]
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.ic_launcher_background) // Set logo drawable
+            .setTheme(R.style.Theme_PetTracer) // Set theme
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_theme_logo]
+    }
+
+    private fun privacyAndTerms() {
+        val providers = emptyList<AuthUI.IdpConfig>()
+        // [START auth_fui_pp_tos]
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTosAndPrivacyPolicyUrls(
+                "https://example.com/terms.html",
+                "https://example.com/privacy.html",
+            )
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_pp_tos]
+    }
+
+    open fun emailLink() {
+        // [START auth_fui_email_link]
+        val actionCodeSettings = ActionCodeSettings.newBuilder()
+            .setAndroidPackageName( // yourPackageName=
+                "com.kennethfechter.lab551", // installIfNotAvailable=
+                true, // minimumVersion=
+                null,
+            )
+            .setHandleCodeInApp(true) // This must be set to true
+            .setUrl("https://google.com") // This URL needs to be whitelisted
+            .build()
+
+        val providers = listOf(
+            AuthUI.IdpConfig.EmailBuilder()
+                .enableEmailLinkSignIn()
+                .setActionCodeSettings(actionCodeSettings)
+                .build(),
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+        // [END auth_fui_email_link]
+    }
+
+    open fun catchEmailLink() {
+        val providers: List<AuthUI.IdpConfig> = emptyList()
+
+        // [START auth_fui_email_link_catch]
+        if (AuthUI.canHandleIntent(intent)) {
+            val extras = intent.extras ?: return
+            val link = extras.getString("email_link_sign_in")
+            if (link != null) {
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setEmailLink(link)
+                    .setAvailableProviders(providers)
+                    .build()
+                signInLauncher.launch(signInIntent)
+            }
+        }
+        // [END auth_fui_email_link_catch]
+    }
+}
---
+++ app/src/main/java/com/kennethfechter/lab551/placeholder.kt
@@ -0,0 +1,4 @@
+package com.kennethfechter.lab551
+
+class placeholder {
+}
